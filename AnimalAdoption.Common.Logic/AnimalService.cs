38c6d3cbe58df5b640501074cc1646fe91577bc6
Alan Wang <alan.wang@ccc.govt.nz>
2020-09-03 11:30:31 +1200

    animal details changes

--- AnimalAdoption.Common.Logic/AnimalService.cs
+++ AnimalAdoption.Common.Logic/AnimalService.cs
@@ -7,9 +7,9 @@ namespace AnimalAdoption.Common.Logic
     public class AnimalService
     {
         public Animal[] ListAnimals => new Animal[] {
-            new Animal { Id = 1, Name = "Sedi", Age = 50, Description = "Soft natured" },
-            new Animal { Id = 2, Name = "Metamorph", Age = 50, Description = "Under a lot of pressure" },
-            new Animal { Id = 3, Name = "Igno", Age = 50, Description = "Shiny and glasslike" },
+            new Animal { Id = 1, Name = "Giraffe", Age = 50, Description = "A new born!" },
+            new Animal { Id = 2, Name = "Dog", Age = 50, Description = "Very happy" },
+            new Animal { Id = 3, Name = "Cat", Age = 50, Description = "Fluffy" },
         };
     }
 }
