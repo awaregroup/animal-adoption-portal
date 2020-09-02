8a944a0b3d135dc467ad530e3aacce8beccb1808
Alan Wang <alan.wang@ccc.govt.nz>
2020-09-03 11:32:57 +1200

    placeholder details change

--- AnimalAdoption.Common.Logic/LoginService.cs
+++ AnimalAdoption.Common.Logic/LoginService.cs
@@ -41,7 +41,7 @@ namespace AnimalAdoption.Common.Logic
                     name = "Murphy";
                     break;
                 case "placeholder":
-                    name = "Placeholder";
+                    name = "Giraffe";
                     break;
             }
             return name;
