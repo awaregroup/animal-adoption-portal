5fa5921585a04da15d4b1cb8b1eac2bd6fc7b75d
Ruskin Dantra <ruskin.dantra@gmail.com>
2020-09-03 15:11:49 +1200

    Added some unit tests

---
+++ AnimalAdoption.Web.Portal.UnitTests/login_service.cs
@@ -0,0 +1,17 @@
+﻿using AnimalAdoption.Common.Logic;
+using FluentAssertions;
+using Xunit;
+
+namespace AnimalAdoption.Web.Portal.UnitTests
+{
+    public class login_service
+    {
+        [Theory]
+        [InlineData("pencil", "Charlie")]
+        public void get_login_user_from_id_should_return_correct_users(string password, string username)
+        {
+            var loginService = new LoginService();
+            loginService.GetLoginUserFromId(password).Should().Be(username);
+        }
+    }
+}
