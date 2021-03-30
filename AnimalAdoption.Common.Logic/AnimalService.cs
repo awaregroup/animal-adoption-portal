a6fd2f25a6cd97161574069a2e5b29017a3f2b7f
An Doo Sung <dsan@lselectric.co.kr>
2021-03-30 11:46:24 +0900

    Update

--- AnimalAdoption.Common.Logic/AnimalService.cs
+++ AnimalAdoption.Common.Logic/AnimalService.cs
@@ -7,9 +7,9 @@ namespace AnimalAdoption.Common.Logic
     public class AnimalService
     {
         public Animal[] ListAnimals => new Animal[] {
-            new Animal { Id = 1, Name = "Sedi", Age = 50, Description = "Soft natured" },
-            new Animal { Id = 2, Name = "Metamorph", Age = 50, Description = "Under a lot of pressure" },
-            new Animal { Id = 3, Name = "Igno", Age = 50, Description = "Shiny and glasslike" },
+            new Animal { Id = 1, Name = "Co", Age = 2, Description = "Cozy" },
+            new Animal { Id = 2, Name = "Zebra", Age = 4, Description = "Luxury" },
+            new Animal { Id = 3, Name = "Racoon", Age = 3, Description = "Fool" },
         };
     }
 }
